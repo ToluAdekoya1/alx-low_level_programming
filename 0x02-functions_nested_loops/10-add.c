15c9fdeb54091155344f4da2068f71254654f3ab
ToluAdekoya1 <toluadekoya05@example.com>
2023-07-19 13:00:05 -0700

    New C file

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * add - Check code
+ * @a: param1
+ * @b: param2
+ * Return: addition of two values
+ */
+int add(int a, int b)
+{
+	int result = a + b;
+
+	return (result);
+}
