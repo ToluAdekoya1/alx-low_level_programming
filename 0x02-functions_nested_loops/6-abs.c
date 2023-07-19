3a0e585d30d2fd0cb9beb277bcdabeee573f7f3e
ToluAdekoya1 <toluadekoya05@example.com>
2023-07-19 01:53:22 -0700

    New C file

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _abs - Check the code
+ * @b: parameter
+ * Return: Always 0
+ */
+int _abs(int b)
+{
+	if (b < 0)
+	{
+		return (-b);
+	}
+	else
+	{
+		return (b);
+	}
+}
