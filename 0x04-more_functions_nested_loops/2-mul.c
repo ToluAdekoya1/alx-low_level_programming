0c9011c71707bb4628ec68c90007b3b78811d462
ToluAdekoya1 <toluadekoya05@example.com>
2023-07-20 05:39:41 -0700

    New c file

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * mul - check the code
+ * @c: param
+ * Result: Always 0
+ */
+
+int mul(int a, int b)
+{
+	if (c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
