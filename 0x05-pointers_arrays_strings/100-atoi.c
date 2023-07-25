d9ea71dbffafe3dc96d880d6fe59bb71ab32941c
ToluAdekoya1 <toluadekoya05@example.com>
2023-07-25 07:55:26 -0700

    Advanced

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -32,15 +32,14 @@ int _atoi(char *s)
 	}
 	while (s[i] >= '0' && s[i] <= '9')
 	{
-		int digit = s[i] - '0';
-
 		if (sign == 1 && (result > INT_MAX / 10 ||
-		 (result == INT_MAX / 10 && digit > INT_MAX % 10)))
+		 (result == INT_MAX / 10 && s[i] - '0' > INT_MAX % 10)))
 		{
 			return (INT_MAX);
 		}
 		else if  (sign == -1 &&
-(result < INT_MIN / 10 || (result == INT_MIN / 10 && digit > -(INT_MIN % 10))))
+			(result < INT_MIN / 10 ||
+			 (result == INT_MIN / 10 && s[i] - '0' > -(INT_MIN % 10))))
 		{
 			return (INT_MIN);
 		}
