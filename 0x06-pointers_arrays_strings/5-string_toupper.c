0ba0a6bac5ec91ab5067e9993a082c4a96327f06
ToluAdekoya1 <toluadekoya05@example.com>
2023-07-27 12:06:52 -0700

    To upper

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * string_toupper - Check the code
+ * @str: created param
+ * Return: always 0
+ */
+
+char *string_toupper(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (str[i] >= 'a' && str[i] <= 'z')
+		{
+			str[i] = str[i] - 32;
+		}
+	}
+	return (str);
+}
