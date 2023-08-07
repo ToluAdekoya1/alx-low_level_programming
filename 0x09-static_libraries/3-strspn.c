d22c4cbda7719283b2966de45c0ecfef8e613ea4
ToluAdekoya1 <siophiex@gmail.com>
2023-08-07 12:04:26 +0100

    Created a static library

--- 0x09-static_libraries/3-strspn.c
+++ 0x09-static_libraries/3-strspn.c
@@ -1,2 +1,26 @@
 #include <stdio.h>
 #include "main.h"
+
+/**
+ * _strspn - Check the code
+ * @s: string to search from
+ * @accept: string to search
+ * Return: numbers of characters matched
+*/
+
+unsigned int _strspn(char *s, char *accept)
+{
+	int i, j;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; s[i] != accept[j]; j++)
+		{
+			if (accept[j] == '\0')
+			{
+				return (i);
+			}
+		}
+	}
+	return (0);
+}
