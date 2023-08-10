0422a747f680ec3011594abd2d25d1f227039ab4
ToluAdekoya1 <siophiex@gmail.com>
2023-08-10 02:03:14 +0100

    concatenates two strings

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -13,9 +13,12 @@ char *str_concat(char *s1, char *s2)
 	char *alloc;
 	int i, count1, count2, size;
 
-	if (s1 == NULL && s2 == NULL)
+	if (s1 == NULL)
 	{
 		s1 = "";
+	}
+	if (s2 == NULL)
+	{
 		s2 = "";
 	}
 	count1 = 0;
